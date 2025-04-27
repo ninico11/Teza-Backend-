9d0d71fc46a169c591cd0c8e8607c51f8b456d7e
ninico11 <ion_rotaru2003@mail.ru>
2025-04-27 09:42:59 +0300

    Added flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask_cors import CORS        
 
 def create_app():
     app = Flask(__name__)
-    
+
+    CORS(app)                        
+
+    # blueprints
     from .apis.communication import communication
     from .apis.audio import audio
     from .apis.sentiment import sentiment
     app.register_blueprint(communication)
     app.register_blueprint(audio)
     app.register_blueprint(sentiment)
-        
+
     return app

01df1335140240a05c211630ee4b8da81998d472
ninico11 <ion_rotaru2003@mail.ru>
2025-05-01 12:57:03 +0300

    Solved the error with audio api

--- app/apis/audio.py
+++ app/apis/audio.py
@@ -1,9 +1,10 @@
 import os
 import tempfile
+import json
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
-from ..tools.audio_trans import transcribe_audio  # Adjust the import according to your project structure
-
+from ..tools.audio_trans import transcribe_audio
+from ..tools.translation import translate_message_event
 audio = Blueprint('audio', __name__)
 
 @audio.route('/voice_to_text', methods=['POST'])
@@ -12,24 +13,26 @@ def voice_to_text():
         return jsonify({"error": "No file provided."}), 400
 
     file = request.files['file']
+    requested_language = request.form.get('requested_language', 'en')
+
     if file.filename == '':
         return jsonify({"error": "No selected file."}), 400
 
     filename = secure_filename(file.filename)
-    
     temp_dir = tempfile.gettempdir()
     temp_path = os.path.join(temp_dir, filename)
-    
     file.save(temp_path)
 
     try:
-        audio = open(temp_path, 'rb')
-        transcription_text = transcribe_audio(audio)
-        audio.close()
+        with open(temp_path, 'rb') as audio_file:
+            content = transcribe_audio(audio_file)
+            translated_text = translate_message_event(content, requested_language)
+            if not isinstance(translated_text, dict):
+                translated_text = json.loads(translated_text)
     except Exception as e:
         return jsonify({"error": f"Transcription failed: {str(e)}"}), 500
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
 
-    return jsonify({"text": transcription_text}), 201
+    return jsonify({"translated_text": translated_text["translated_message"], "original": content}), 201
